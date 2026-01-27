4924f69ec767f4655b49c397a96ab82c4b0faa59
Alex <alexodwright@gmail.com>
2026-01-27 11:11:29 +0000

    clean test_model.c

--- core/dev/test_model.c
+++ core/dev/test_model.c
@@ -4,43 +4,21 @@
 
 #include <stdio.h>
 
-Mesh at_model_to_raylib_mesh(const AT_Model *model)
+#define WINDOW_WIDTH 1200
+#define WINDOW_HEIGHT 1200
+
+AT_Triangle get_triangle_n_from_model(const AT_Model *model, uint32_t i)
 {
-    Mesh mesh = {0};
-
-    mesh.vertexCount   = model->vertex_count;
-    mesh.triangleCount = model->index_count / 3;
-
-    // Allocate Raylib buffers
-    mesh.vertices = MemAlloc(sizeof(float) * 3 * mesh.vertexCount);
-    mesh.normals  = MemAlloc(sizeof(float) * 3 * mesh.vertexCount);
-    mesh.indices  = MemAlloc(sizeof(unsigned int) * model->index_count);
-
-    // Vertices
-    for (int i = 0; i < model->vertex_count; i++) {
-        mesh.vertices[i*3 + 0] = model->vertices[i].x;
-        mesh.vertices[i*3 + 1] = model->vertices[i].y;
-        mesh.vertices[i*3 + 2] = model->vertices[i].z;
-    }
-
-    // Normals
-    for (int i = 0; i < model->vertex_count; i++) {
-        mesh.normals[i*3 + 0] = model->normals[i].x;
-        mesh.normals[i*3 + 1] = model->normals[i].y;
-        mesh.normals[i*3 + 2] = model->normals[i].z;
-    }
-
-    for (int i = 0; i < model->index_count; i++) {
-        mesh.indices[i] = model->indices[i];
-    }
-
-    UploadMesh(&mesh, false);
-    return mesh;
+    return (AT_Triangle){
+    {model->vertices[model->indices[i*3]].x, model->vertices[model->indices[i*3]].y, model->vertices[model->indices[i*3]].z},
+    {model->vertices[model->indices[i*3 + 1]].x, model->vertices[model->indices[i*3 + 1]].y, model->vertices[model->indices[i*3 + 1]].z},
+    {model->vertices[model->indices[i*3 + 2]].x, model->vertices[model->indices[i*3 + 2]].y, model->vertices[model->indices[i*3 + 2]].z}
+    };
 }
 
 void draw_model(const AT_Model *model)
 {
-    InitWindow(1280, 720, "Test");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Test");
     Camera3D camera = {
         .position = { 10.0f, 10.0f, 10.0f },
         .target = { 0.0f, 0.0f, 0.0f },
@@ -50,14 +28,20 @@ void draw_model(const AT_Model *model)
     };
     SetTargetFPS(60);
 
-    Model rl_model = LoadModelFromMesh(at_model_to_raylib_mesh(model));
-
     while(!WindowShouldClose()) {
         UpdateCamera(&camera, CAMERA_FREE);
         BeginDrawing();
             ClearBackground(RAYWHITE);
             BeginMode3D(camera);
-                DrawModel(rl_model, (Vector3){ 0.0f, 0.0f, 0.0f }, 1, GREEN);
+                for (uint32_t i = 0; i < model->index_count / 3; i++) {
+                    AT_Triangle triangle = get_triangle_n_from_model(model, i);
+                    DrawTriangle3D(
+                        (Vector3){triangle.v1.x, triangle.v1.y, triangle.v1.z},
+                        (Vector3){triangle.v2.x, triangle.v2.y, triangle.v2.z},
+                        (Vector3){triangle.v3.x, triangle.v3.y, triangle.v3.z},
+                        GREEN
+                        );
+                }
                 DrawGrid(10, 1.0f);
             EndMode3D();
             DrawFPS(10, 10);
@@ -65,6 +49,7 @@ void draw_model(const AT_Model *model)
     }
     CloseWindow();
 }
+
 int main()
 {
     const char *filepath = "../assets/glb/L_room.gltf";
