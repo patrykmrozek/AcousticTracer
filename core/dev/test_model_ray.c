b1d7b0c76db404bd00ecab3d8b866756c49741dd
patrykmrozek <45935681+patrykmrozek@users.noreply.github.com>
2026-01-27 10:39:45 +0000

    render intersections between all rays and model

--- core/dev/test_model_ray.c
+++ core/dev/test_model_ray.c
@@ -10,6 +10,8 @@
 #include <stdio.h>
 #include <float.h>
 
+#define MAX_RAYS 50
+
 AT_Triangle *AT_model_get_triangles(const AT_Model *model)
 {
     uint32_t triangle_count = model->index_count / 3;
@@ -23,23 +25,36 @@ AT_Triangle *AT_model_get_triangles(const AT_Model *model)
     }
     return ts;
 }
-AT_RayHit AT_ray_get_closest_hit(const AT_Ray *ray)
-{
-    AT_RayHit closest_hit = {0};
-    float smallest_dist = FLT_MAX;
-    for (uint32_t i = 0; i < ray->hits.count; i++) {
-        float curr_dist = AT_vec3_distance_sq(ray->origin, ray->hits.items[i].position);
-        if (curr_dist < smallest_dist) {
-            smallest_dist = curr_dist;
-            closest_hit = ray->hits.items[i];
-        }
-    }
-    return closest_hit;
-}
+
+// AT_RayHit AT_ray_get_closest_hit(const AT_Ray *ray)
+// {
+//     AT_RayHit closest_hit = {0};
+//     float smallest_dist = FLT_MAX;
+//     for (uint32_t i = 0; i < ray->hits.count; i++) {
+//         float curr_dist = AT_vec3_distance_sq(ray->origin, ray->hits.items[i].position);
+//         if (curr_dist < smallest_dist) {
+//             smallest_dist = curr_dist;
+//             closest_hit = ray->hits.items[i];
+//         }
+//     }
+//     //*dist = smallest_dist;
+//     return closest_hit;
+
+//     /*
+//     AT_RayHit closest_hit = {0};
+//     float min_dist = FLT_MAX;
+//     for (uint32_t i = 0; i < ray->hits.count; i++) {
+//         if (ray->hits.items[i].t < min_dist) {
+//             closest_hit = ray->hits.items[i];
+//         }
+//     }
+//     return closest_hit;
+//     */
+// }
 
 int main()
 {
-    const char *filepath = "../assets/glb/box_room.gltf";
+    const char *filepath = "../assets/glb/L_room.gltf";
 
     AT_Model *model = NULL;
     if (AT_model_create(&model, filepath) != AT_OK) {
@@ -47,21 +62,32 @@ int main()
         return 1;
     }
 
-    AT_Ray ray = AT_ray_init(
-        (AT_Vec3){0.5f, 0.5, 10.0f},
-        (AT_Vec3){0.0f, 0.0f, -1.0f},
-        0
-    );
+    AT_Ray rays[MAX_RAYS] = {0};
+
+    //init rays
+    for (uint32_t i = 0; i < MAX_RAYS; i++) {
+        rays[i] = AT_ray_init(
+            (AT_Vec3){(float)i*0.05-1, 0.5, 10.0f},
+            (AT_Vec3){i*-0.001, 0.0f, -1.0f},
+            0);
+    }
 
     uint32_t t_count = model->index_count / 3;
     AT_Triangle *ts = AT_model_get_triangles(model);
 
-    AT_RayHit hit = {0};
-
-    for (uint32_t i = 0; i < t_count; i++) {
-        if (AT_ray_triangle_intersect(&ray, &ts[i], &hit)) {
-            AT_ray_add_hit(&ray, hit);
-            printf("HIT!\n");
+    //iterate rays
+    for (uint32_t i = 0; i < MAX_RAYS; i++) {
+        AT_RayHit hit = {0};
+        for (uint32_t j = 0; j < t_count; j++) {
+            printf("Checking Ray %i\n", i);
+            if (AT_ray_triangle_intersect(&rays[i], &ts[j], &hit)) {
+                AT_ray_add_hit(&rays[i], hit);
+                printf("HIT! Ray %i {%.2f, %.2f, %.2f}\n",
+                    i,
+                    hit.position.x,
+                    hit.position.y,
+                    hit.position.z);
+            }
         }
     }
 
@@ -86,10 +112,28 @@ int main()
             ClearBackground(RAYWHITE);
             BeginMode3D(camera);
             {
-                DrawRay((Ray){
-                (Vector3){ray.origin.x, ray.origin.y, ray.origin.z},
-                (Vector3){ray.direction.x, ray.direction.y, ray.direction.z}
-                }, RED);
+                //draw all rays
+                for (uint32_t i = 0; i < MAX_RAYS; i++) {
+                    DrawSphere((Vector3){
+                    rays[i].origin.x,
+                    rays[i].origin.y,
+                    rays[i].origin.z},
+                    0.1, RED);
+
+                    DrawRay((Ray){
+                    (Vector3){rays[i].origin.x, rays[i].origin.y, rays[i].origin.z},
+                    (Vector3){rays[i].direction.x, rays[i].direction.y, rays[i].direction.z}
+                    }, RED);
+
+                    for (uint32_t j = 0; j < rays[i].hits.count; j++) {
+                        DrawSphere((Vector3){
+                                rays[i].hits.items[j].position.x,
+                                rays[i].hits.items[j].position.y,
+                                rays[i].hits.items[j].position.z},
+                                0.1, RED);
+                    }
+
+                }
 
                 for (uint32_t i = 0; i < t_count; i++) {
                     DrawTriangle3D(
@@ -98,23 +142,8 @@ int main()
                         (Vector3){ts[i].v3.x, ts[i].v3.y, ts[i].v3.z},
                         GREEN);
                 }
+
                 DrawGrid(10, 1.0f);
-
-                //ray origin
-                DrawSphere((Vector3){
-                    ray.origin.x,
-                    ray.origin.y,
-                    ray.origin.z},
-                    0.1, RED);
-
-                AT_RayHit closest_hit = AT_ray_get_closest_hit(&ray);
-                DrawSphere(
-                    (Vector3){
-                        closest_hit.position.x,
-                        closest_hit.position.y,
-                        closest_hit.position.z},
-                    0.1, BLUE);
-
             }
             EndMode3D();
             DrawFPS(10, 10);
